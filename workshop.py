f4e99812586aa8bb6c4a4fae9bd02e5dc347bd58
GurjotSinghArora <94769087+GurjotSinghArora@users.noreply.github.com>
2022-05-03 20:26:47 -0400

    Update workshop.py

--- workshop.py
+++ workshop.py
@@ -1,4 +1,8 @@
 a1,a2=[int(x) for x in input("Please enter 2 numbers:").split()]
 a1=a1**3
 a2 +=2
-print(a1,a2)
+result = a1*a2
+print(result)
+a3=input("Enter the third  input")
+output=result*int(a3)
+print(output)
