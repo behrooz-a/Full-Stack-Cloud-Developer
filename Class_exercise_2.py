90c41b607a7120990cb38efc0d9f1405bd8335f5
Behrooz <90620602+behrooz-a@users.noreply.github.com>
2022-05-10 22:43:59 -0400

    Update Class_exercise_2.py

--- Class_exercise_2.py
+++ Class_exercise_2.py
@@ -3,19 +3,24 @@ GRADE=[]
 while True:
     INPUT=input("enter S for search or A for add:")
 
-    if INPUT=='A':
-       a1=input("Please enter your name  :")
-       a2=input("Please enter your grade  :")
-       NAME.append(a1)
-       GRADE.append(a2)
 
-    elif INPUT=='S':
-         LENGTH=len(NAME)
-         b1=int(input("Please the index or ID of student:"))
-         if b1>LENGTH :
-               print('Enter a correct number')
-         else:
-               print('The grade for '+(NAME[b1],'is',GRADE[b1] )
+    if INPUT=='S':
+        LENGTH=len(NAME)
+        b1=int(input("Please enter the index or ID of student:"))
+
+        if b1>LENGTH :
+            print('Enter a correct number')
+        else:
+            print('The grade for',NAME[b1],'is',GRADE[b1] )
+        
+                  
+    elif INPUT=='A':
+        a1=input("Please enter your name  :")
+        a2=input("Please enter your grade  :")
+        NAME.append(a1)
+        GRADE.append(a2)
+       
     else: 
         print("Sorry, I did not understand that. Please try it again.")
         continue
+    
